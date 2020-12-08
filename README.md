3dbb3fbbae09fd6675f87225f5044797575dfb9e
toshiyuki <toshiyuki.saito.16@gmail.com>
2020-12-09 08:11:49 +0900

    #02 make development env and implement backend code

---
+++ README.md
@@ -0,0 +1,1 @@
+# layr_tutorial
---
+++ src/backend.ts
@@ -0,0 +1,13 @@
+import { Component, attribute, method, expose } from "@layr/component"
+import {ComponentHTTPServer } from "@layr/component-http-server"
+
+export class Greeter extends Component {
+  @expose({set: true}) @attribute("string") name = "World";
+  @expose({call: true}) @method() async hello() {
+    return `Hello ${this.name}!`;
+  }
+}
+
+const server = new ComponentHTTPServer(Greeter, {port: 3210});
+server.start();
+
