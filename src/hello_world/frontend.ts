6b54af4f901c233eabac148eb34788e48a668d43
toshiyuki <toshiyuki.saito.16@gmail.com>
2020-12-10 08:14:23 +0900

    #02 implements hello_world frontend

---
+++ src/hello_world/frontend.ts
@@ -0,0 +1,19 @@
+import {ComponentHTTPClient} from '@layr/component-http-client';
+import type {Greeter as GreeterType} from './backend';
+
+(async () => {
+
+  const client = new ComponentHTTPClient("http://localhost:3210");
+
+  const BackendGreeter = (await client.getComponent()) as typeof GreeterType;
+
+  class Greeter extends BackendGreeter {
+    async hello() {
+      return (await super.hello()).toUpperCase();
+    }
+  }
+
+  const greeter = new Greeter({name: "Steve"});
+
+  console.log(await greeter.hello());
+})();
